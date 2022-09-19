69704463d7b85fb4a20a7326a3bcd9dda8c65784
EvelynY16 <108499213+EvelynY16@users.noreply.github.com>
2022-09-19 12:48:57 +0900

    Add files via upload

---
+++ LAB1_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) 
+{
+	//여기에 코드를 작성하시오.
+	
+	printf("I love \tDongduk!!\n");
+
+	return 0;
+}
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello\nWorld!\næ»≥Á«œººø‰");
+	
+	return 0;
+}
+
